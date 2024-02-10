a8bb2d20ea1598c9ed0ba6d23fbb9a8b6e2af2dc
Ishan Joshi <ishanjoshi205@gmail.com>
2024-02-10 13:03:01 -0600

    Cleaned up code, changed initial prompt

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from Game import ChessGame
 import json
+import os
 import random
-import pygame
-import sys
+from dotenv import load_dotenv
 import cohere
 import re
 
+def init_llm():
+  load_dotenv()
+  cohere_llm = cohere.Client(os.getenv("COHERE_API_KEY"))
 
-def predict_move(input_prompt, fen_string):
+  return cohere_llm
+
+def predict_move(input_prompt, fen_string, llm):
   return_uci = ""
-  co = cohere.Client("2ZXcGcEyP7s2SBm8gunpe3HDRQQPXSF6hbDTl5jS")
-  prompt = f"{input_prompt} {'with the fen string: '} {fen_string}"
-  response = co.generate(prompt = prompt)
+  
+  prompt = f"{input_prompt} {' fen string: '} {fen_string}"
+  response = llm.generate(prompt = prompt)
   # print("response text: ", response[0].text)
 
   uci_pattern = r"\b[a-h][1-8][a-h][1-8][nbrq]?\b"
@@ -19,23 +24,23 @@ def predict_move(input_prompt, fen_string):
 
   if uci_matches:
     first_uci_match = uci_matches[0]
-    # print("first match found:", first_uci_match)
     return_uci = first_uci_match
   else:
     print("No UCI match found")
 
   # Iterate over the matches and print each one
-  for matched_text in uci_matches:
-      print("Match found:", matched_text)
+  # for matched_text in uci_matches:
+  #     print("Match found:", matched_text)
   
-  print("return uci", return_uci)
   return return_uci
 
 if __name__ == "__main__":
   data = json.loads(open("PGNs/fens.json", "r").read())
 
+  cohere_llm = init_llm()
+
   # initial prompt
-  prompt = "Given a FEN string return the next best chess move in UCI format"
+  prompt = "Given a FEN string and legal moves, return the next best move among the given moves in UCI format. "
 
   total_games = 1
 
@@ -47,16 +52,19 @@ if __name__ == "__main__":
     batch = 1
     show_interval = 1000
 
+    print("fen:", game.get_board().fen())
+
     for step in range(steps):
       total_score = 0
 
       for idx in range(batch):
         # predict move with a LLM using prompt and board fen. Validate move, then play it
-        # move = random.choice(list(game.get_board().legal_moves)).uci()
 
-        move = predict_move(prompt, fen)
+        # move = random.choice(list(game.get_board().legal_moves)).uci()
+        prompt += "moves: " + " ".join([move.uci() for move in game.get_board().legal_moves])
+        move = predict_move(prompt, fen, cohere_llm)
         
-        print(move)
+        print("move:", move)
 
         # Show the board at these intervals, wait for input to continue
         if (idx+1) % show_interval == 0:
