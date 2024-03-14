ad0d29537d91817672159d3902ff4748f1289e87
DJ <dhrubajyotipaul3@gmail.com>
2024-03-14 08:42:22 -0500

    done setting up gemini

--- main.py
+++ main.py
@@ -8,14 +8,20 @@ import cohere
 from openai import OpenAI
 import google.generativeai as genai
 import time
+from tqdm import tqdm
+
 
 def init_llm():
     load_dotenv()
     cohere_llm = cohere.Client(api_key=os.getenv("COHERE_API_KEY"))
     openai_llm = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-    genai_llm = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+    genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+    gemini_llm = genai.GenerativeModel(
+        model_name="gemini-1.0-pro-latest",
+        generation_config=genai.GenerationConfig(temperature=1, max_output_tokens=2048),
+    )
 
-    return cohere_llm, openai_llm, genai_llm
+    return cohere_llm, openai_llm, gemini_llm
 
 
 def predict_move_cohere(input_prompt, fen_string, llm):
@@ -50,7 +56,7 @@ def predict_move_openai(input_prompt, moves_till_now, legal_moves, llm, num):
             {"role": "system", "content": prompt},
         ],
         n=num,
-        temperature=1.0
+        temperature=1.0,
     )
 
     responses = []
@@ -125,7 +131,90 @@ def get_prompt_openai(prompt_score_list, example_data, llm):
     return return_str
 
 
-def run(steps, total_games, num, openai_llm, data, show_interval=1000, batch=1):
+def predict_move_gemini(input_prompt, moves_till_now, legal_moves, llm, num):
+    prompt = f"{input_prompt} {'Moves played till now:'} {moves_till_now} {'Legal moves:'} {legal_moves}"
+
+    # print(prompt)
+
+    # Add num different responses. Model Temp = 1
+    responses = set()
+    while len(responses) < num:
+        res = None
+        while True:
+            try:
+                res = gemini_llm.generate_content(prompt).candidates[0].content.parts[0].text
+                break
+            except:
+                continue
+        responses.add(res)
+
+    # print("move responses: ", responses)
+
+    san_pattern = r"\b(?:[NBRQK]?[a-h]?[1-8]?x?[a-h][1-8][+#]?|O-O(?:-O)?|0-0(?:-0)?)\b"
+
+    return_sans = []
+
+    for resp in responses:
+        san_matches = re.findall(san_pattern, resp)
+
+        if san_matches:
+            first_san_match = san_matches[0]
+            return_sans.append(first_san_match)
+        else:
+            # print("No SAN match found")
+            return_sans.append("0000")
+
+    return return_sans
+
+
+def get_prompt_gemini(prompt_score_list, example_data, llm):
+    prompt_list = ""
+    examples = ""
+
+    for idx, prompts in enumerate(prompt_score_list):
+        prompt_list += (
+            "\nPrompt "
+            + str(idx + 1)
+            + ":\n"
+            + prompts[0]
+            + "\nScore: "
+            + str(prompts[1])
+            + "\n"
+        )
+
+    for idx, example in enumerate(example_data):
+        examples += (
+            "\nMoves played till now: "
+            + example[0]
+            + "\nLegal moves: "
+            + example[1]
+            + "\nBest move: "
+            + example[2]
+            + "\n"
+        )
+
+    # https://arxiv.org/pdf/2309.03409.pdf
+    prompt = f"""Your task is to generate a prompt that will achieve a higher score than the given prompts. A higher score means a better move prediction. Below are some prompts to play Chess and the score the prompt achieved.
+  {prompt_list}\nBelow are some examples of the input and the best answer. \n{examples} \nGenerate a prompt that will achieve a higher score than the given prompts. A higher score means a better move prediction. the prompt should begin with "Prompt:" and should not include anything else.
+  """
+
+    # print(prompt)
+    response = None
+    while True:
+        try:
+            response = gemini_llm.generate_content(prompt).candidates[0].content.parts[0].text
+            break
+        except:
+            continue
+
+    return_str = [x for x in response.split(":")[::-1] if len(x) > 4][0]
+
+    # print("new prompt response: ", return_str)
+
+    return return_str
+
+
+def run(steps, total_games, num, llm, data, show_interval=1000, batch=1):
     # prompt list, only include recent 5 prompts and scores
     prompt_score_list = []
 
@@ -135,7 +224,7 @@ def run(steps, total_games, num, openai_llm, data, show_interval=1000, batch=1):
     # initial prompt
     prompt = "Given the moves played till now and a list of legal moves, select and return only the next best move from the given list of legal moves in SAN format and nothing else."
 
-    for _ in range(steps):
+    for _ in tqdm(range(steps)):
         example_data = []
         total_score = 0
 
@@ -163,8 +252,8 @@ def run(steps, total_games, num, openai_llm, data, show_interval=1000, batch=1):
 
                 # move = random.choice(list(game.get_board().legal_moves)).uci()
                 # move = predict_move_cohere(prompt, fen, cohere_llm)
-                moves = predict_move_openai(
-                    prompt, moves_till_now, legal_moves, openai_llm, num
+                moves = predict_move_gemini(
+                    prompt, moves_till_now, legal_moves, llm, num
                 )
 
                 for move in moves:
@@ -201,7 +290,7 @@ def run(steps, total_games, num, openai_llm, data, show_interval=1000, batch=1):
         example_data = example_data[-5:]
 
         # new_prompt = "Given a FEN string return the next best chess move in UCI format"
-        new_prompt = get_prompt_openai(prompt_score_list, example_data, openai_llm)
+        new_prompt = get_prompt_gemini(prompt_score_list, example_data, llm)
 
         output_str += f"Current Prompt: {prompt}\n"
         output_str += f"Average score: {avg_score}\n"
@@ -213,19 +302,23 @@ def run(steps, total_games, num, openai_llm, data, show_interval=1000, batch=1):
     output_str += "done"
     return output_str, avg_score
 
+
 if __name__ == "__main__":
     data = json.loads(open("PGNs/data.json", "r").read())
 
     cohere_llm, openai_llm, gemini_llm = init_llm()
 
+    # Default Params
     show_interval = 1000
     batch = 1
+
+    # Adjustable Params
     steps = 3
     total_games = 2
     num = 5
-    
-    # Run the game
+
+    # Run the game for prompts
     start_time = time.time()
-    output_str, avg_score = run(steps, total_games, num, openai_llm, data)  # batch = 1, show_interval = 1000 by default
+    output_str, avg_score = run(steps, total_games, num, gemini_llm, data)  # batch = 1, show_interval = 1000 by default
     end_time = time.time()
     print(output_str, avg_score, end_time-start_time)
